73941b22565ad1ea071ce73b9e3d28fd77a640e9
Raymond Oh <raymond.oh@gmail.com>
2020-10-15 17:32:03 -0400

    created API for kittens resource in backend

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,11 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html 
+      format.json { render json: @kittens }
+    end
   end
 
   def new
@@ -29,6 +34,11 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html 
+      format.json { render json: @kitten }
+    end
   end
 
   def destroy
