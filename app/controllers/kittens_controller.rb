0c52374f236137c4e8701d8a55a937e51fe548d5
Raymond Oh <raymond.oh@gmail.com>
2020-10-15 16:19:34 -0400

    establish views and controller actions for basic kitten CRUD capability

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,44 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    @kitten.save
+
+    redirect_to kitten_path(@kitten)
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update(kitten_params)
+
+    redirect_to kitten_path(@kitten)
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
